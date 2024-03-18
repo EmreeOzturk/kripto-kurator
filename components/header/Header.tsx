7d55b00e7132068c6a981ce29be68d14addd22ef
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-19 02:21:53 +0300

    Add links to header menu

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,6 +3,7 @@ import logo from '@/public/assets/logo.png'
 import SocialLogos from './SocialLogos';
 import ConnectWalletButton from './ConnectWalletButton';
 import KriptoKurator from './KriptoKurator';
+import Link from 'next/link';
 
 const Header = () => {
     return (
@@ -11,15 +12,21 @@ const Header = () => {
             backdrop-blur-3xl w-[90%] m-auto bg-gray-900/30 z-20 fixed top-0 right-0 left-0' >
             <div className='flex'>
                 <ul className='flex gap-2  text-purple-300 text-lg cursor-pointer'>
-                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl '>Collection</li>
-                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl'>Roadmap</li>
-                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl'>Team</li>
+                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl '>
+                        <Link href='/'>Collection</Link>
+                    </li>
+                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl'>
+                        <Link href='#roadmap'>Roadmap</Link>
+                    </li>
+                    <li className='hover:bg-slate-200/20 p-2 flex items-center hover:scale-105 transition-all rounded-xl'>
+                        <Link href='#team'>Team</Link>
+                    </li>
                 </ul>
             </div>
-            <div className='flex justify-center items-center flex-1'>
+            <Link href="/" className='flex justify-center items-center flex-1'>
                 <Image src={logo} alt='logo' height={50} sizes='100vw' />
                 <KriptoKurator />
-            </div>
+            </Link>
             <div className='flex items-center justify-center gap-2'>
                 <ConnectWalletButton />
                 <SocialLogos />
