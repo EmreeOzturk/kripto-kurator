7a36a245f4defe819605d5381d0f5e770987a197
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-19 02:23:30 +0300

    Update social media links in header component

--- components/header/SocialLogos.tsx
+++ components/header/SocialLogos.tsx
@@ -2,20 +2,29 @@ import Insta from '@/svg/insta.svg'
 import Substack from '@/svg/substack.svg'
 import Twitter from '@/svg/twitter.svg'
 import Discord from '@/svg/discord.svg'
+import Link from 'next/link'
 const SocialLogos = () => {
     return (
         <>
             <div className='p-1 border-2 border-purple-300 rounded-full'>
-                <Insta />
+                <Link href='https://www.instagram.com/kriptokurator/'>
+                    <Insta />
+                </Link>
             </div>
             <div className='p-1 border-2 border-purple-300 rounded-full'>
-                <Substack />
+                <Link href='https://kriptokurator.substack.com/'>
+                    <Substack />
+                </Link>
             </div>
             <div className='p-1 border-2 border-purple-300 rounded-full'>
-                <Twitter />
+                <Link href='https://twitter.com/kriptokurator'>
+                    <Twitter />
+                </Link>
             </div>
             <div className='p-1 border-2 border-purple-300 rounded-full'>
-                <Discord />
+                <Link href='https://discord.gg/'>
+                    <Discord />
+                </Link>
             </div>
         </>
     )
